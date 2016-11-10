b93d8cf192dd048dc641dfaaac858bb24f15cb11
Prakhar Shukla <prafiles@yahoo.com>
2016-11-10 09:27:13 +0000

    update to cmdline to improve cert strength etc

--- cronjobs/letsencrypt-refresh_https_certs.sh
+++ cronjobs/letsencrypt-refresh_https_certs.sh
@@ -7,8 +7,8 @@ service nginx stop
 
 cd /root/backup/letsencrypt
 
-./letsencrypt-auto certonly --standalone --keep-until-expiring -d "www.srikrishnaacharya.in,srikrishnaacharya.in"
+./letsencrypt-auto certonly --standalone --force-renewal --expand --rsa-key-size 4096 --staple-ocsp --email "prafiles@yahoo.com" -d "www.srikrishnaacharya.in,srikrishnaacharya.in"
 
-./letsencrypt-auto certonly --standalone --keep-until-expiring -d "prakharshukla.com,www.prakharshukla.com,bio.arxiv-sanity.prakharshukla.com,phy.arxiv-sanity.prakharshukla.com"
+./letsencrypt-auto certonly --standalone --force-renewal --expand --rsa-key-size 4096 --staple-ocsp --email "prafiles@yahoo.com" -d "prakharshukla.com,www.prakharshukla.com,bio.arxiv-sanity.prakharshukla.com,phy.arxiv-sanity.prakharshukla.com"
 
 service nginx start
