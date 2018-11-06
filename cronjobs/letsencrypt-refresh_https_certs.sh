df43f57cdc091d676813ba6a6074b327e6330411
Prakhar Shukla <prafiles@yahoo.com>
2018-11-06 13:39:28 +0530

    removing sudo

--- cronjobs/letsencrypt-refresh_https_certs.sh
+++ cronjobs/letsencrypt-refresh_https_certs.sh
@@ -3,5 +3,5 @@
 
 PATH=/usr/local/bin/ices:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH #Updating path for CronJob
 
-sudo certbot --nginx --force-renewal --no-redirect --expand --rsa-key-size 4096 -m "prafiles@yahoo.com" -d "www.srikrishnaacharya.in,srikrishnaacharya.in"
-sudo certbot --nginx --force-renewal --no-redirect --expand --rsa-key-size 4096 -m "prafiles@yahoo.com" -d "prakharshukla.com,www.prakharshukla.com,api.prakharshukla.com,blog.prakharshukla.com,demo.prakharshukla.com"
+certbot --nginx --force-renewal --no-redirect --expand --rsa-key-size 4096 -m "prafiles@yahoo.com" -d "www.srikrishnaacharya.in,srikrishnaacharya.in"
+certbot --nginx --force-renewal --no-redirect --expand --rsa-key-size 4096 -m "prafiles@yahoo.com" -d "prakharshukla.com,www.prakharshukla.com,api.prakharshukla.com,blog.prakharshukla.com,demo.prakharshukla.com"
