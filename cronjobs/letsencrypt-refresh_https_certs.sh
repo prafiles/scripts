7c86c2cfd5b137bd902cc2fbba9efa224205332c
Prakhar Shukla <prafiles@yahoo.com>
2018-11-06 13:39:06 +0530

    fix for new certbot

--- cronjobs/letsencrypt-refresh_https_certs.sh
+++ cronjobs/letsencrypt-refresh_https_certs.sh
@@ -3,12 +3,5 @@
 
 PATH=/usr/local/bin/ices:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH #Updating path for CronJob
 
-service nginx stop
-
-cd /root/backup/letsencrypt
-
-./letsencrypt-auto certonly --standalone --force-renewal --expand --rsa-key-size 4096 --staple-ocsp --email "prafiles@yahoo.com" -d "www.srikrishnaacharya.in,srikrishnaacharya.in"
-
-./letsencrypt-auto certonly --standalone --force-renewal --expand --rsa-key-size 4096 --staple-ocsp --email "prafiles@yahoo.com" -d "prakharshukla.com,www.prakharshukla.com,api.prakharshukla.com,blog.prakharshukla.com,bio.arxiv-sanity.prakharshukla.com,phy.arxiv-sanity.prakharshukla.com"
-
-service nginx start
+sudo certbot --nginx --force-renewal --no-redirect --expand --rsa-key-size 4096 -m "prafiles@yahoo.com" -d "www.srikrishnaacharya.in,srikrishnaacharya.in"
+sudo certbot --nginx --force-renewal --no-redirect --expand --rsa-key-size 4096 -m "prafiles@yahoo.com" -d "prakharshukla.com,www.prakharshukla.com,api.prakharshukla.com,blog.prakharshukla.com,demo.prakharshukla.com"
